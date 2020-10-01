fa2d19347a108a4b412874bf8dfaf4ffa0aa4296
kamral <kamral010101@gmail.com>
2020-10-01 21:07:37 +0300

    initial

--- blog_apiii/settings.py
+++ blog_apiii/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '-!uc=o!*pxehkl(iry5o$e)0o%hkv8&9a%3t_#l2=!vjbgh-qq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
